5667a9619cb88c5ffaeb6cb5ddaf85d5efc094f8
FelixTheWolfrick <felix.abbott@mymail.champlain.edu>
2020-12-03 15:07:03 -0500

    Added basic circular orbit, tried figuring out math needed for elliptical orbit

---
+++ SolarSystem/Assets/Scripts/Felix/OrbitControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitControl : MonoBehaviour
+{
+    //Variables
+    public GameObject sun;
+
+    public float orbitSpeed;
+    public float gravity;
+
+    /*Formula Notes
+    Orbital Eccentricity Calculations
+    0 - ciruclar // 1 - elliptical
+    E = (a - p) / (a + p) // a - point of closest approach(aphelion), p - point of greatest distance(perihelion)*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Last Update so if the sun ever moves, it'll move after it's moved
+    void LateUpdate()
+    {
+        Orbit();
+    }
+
+    void Orbit()
+    {
+
+    }
+}
---
+++ SolarSystem/Assets/Scripts/Felix/OrbitReference.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitReference : MonoBehaviour
+{
+    //Variables
+    public GameObject sun;
+    public float orbitDistance = 10.0f;
+    public float orbitDegreesPerSec = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Use late update to make sure planet moves after sun has moved (if we add something like that)
+    void LateUpdate()
+    {
+        Orbit();
+    }
+
+    //Orbit
+    void Orbit()
+    {
+        if(sun != null)
+        {
+            //Make sure to keep orbit distance from sun
+            transform.position = sun.transform.position + (transform.position - sun.transform.position).normalized * orbitDistance;
+            transform.RotateAround(sun.transform.position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
+        }
+    }
+}
