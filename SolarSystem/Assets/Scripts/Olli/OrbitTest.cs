48fd02f7cfe55b49f59fc007c83560b210f5f33a
Olli Machina <olli.machina@mymail.champlain.edu>
2020-12-05 10:49:41 -0700

    adding work on math for orbit

---
+++ SolarSystem/Assets/Scripts/Olli/OrbitTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitTest : MonoBehaviour
+{
+    Planet currentOrbit;
+    public Planet sun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentOrbit = GetComponent<Planet>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Orbit();
+    }
+
+    void Orbit()
+    {
+        double distanceAcc = calculateDistanceAcc(currentOrbit);
+        currentOrbit.distSpeed = newValue(currentOrbit.distSpeed, distanceAcc);
+        currentOrbit.distance = newValue(currentOrbit.distance, currentOrbit.distSpeed);
+
+        double angleAcc = calculateAngleAcc(currentOrbit);
+        currentOrbit.angleSpeed = newValue(currentOrbit.angleSpeed, angleAcc);
+        currentOrbit.angle = newValue(currentOrbit.angle, currentOrbit.angleSpeed);
+    }
+
+    public double calculateDistanceAcc(Planet calcPlanet) //forces between due to gravity
+    {
+        double distance = calcPlanet.distance;
+        double angleSpeed = calcPlanet.angleSpeed;
+        double gravity = calcPlanet.gravitationalConstant * sun.mass;
+
+        double acc = distance * Mathf.Pow((float)angleSpeed, 2) - gravity / Mathf.Pow((float)distance, 2);
+
+        return acc;
+    }
+
+    public double calculateAngleAcc(Planet calcPlanet)
+    {
+        return (-2.0 * calcPlanet.distSpeed * calcPlanet.angleSpeed / calcPlanet.distance);
+    }
+
+    public double newValue(double current, double derivative)
+    {
+        return (current + Time.deltaTime * derivative);
+    }
+}
---
+++ SolarSystem/Assets/Scripts/Olli/Planet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public double distance, distSpeed, angleSpeed;
+    public double mass, gravitationalConstant, angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
